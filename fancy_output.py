c4ea7197f3d11138dacc054b1f275669677531c6
k-eks <kreks@gmx.at>
2015-05-08 09:04:45 +0200

    module for "visual" display of messages

---
+++ fancy_output.py
@@ -0,0 +1,7 @@
+def okay(message):
+    """Just a fancy way to show that everything is working fine."""
+    print ("\033[92m" + message + "\033[0m")
+    
+def error(message):
+    """Just a fancy way to show that an error occured."""
+    print ("\033[91m" + message + "\033[0m")
