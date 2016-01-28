b3c2f29039ba26837d2b76a530821e55d38dc896
k-eks <kreks@gmx.at>
2016-01-28 19:08:18 +0100

    Updated in-line docu

--- xds_tools.py
+++ xds_tools.py
@@ -31,8 +31,12 @@ def bgFromH5(pathToFile, frameShape):
 
 
 def generate_all_unwanted_pixel(bgFrame, hotStart):
-    """This function reads out all pixels which do not belong to the data set into linear arrays."""
-    """These linear arrays can be used for fancy indexing."""
+    """This function reads out all pixels which do not belong to the data set into linear arrays.
+    These linear arrays can be used for fancy indexing.
+    bgFrame ... fabio.Image background frame from which untrusted, defetive and hot pixels can be extracted
+    hotStart ... threshold value from which pixels should be considered as hot
+    returns numpy.array1d, numpy.array1d, numpy.array1d one-dimensional representation of the location of untrusted, defective and hot pixels
+    """
     defectivePixel = []
     untrustedPixel = []
     hotPixel = []
@@ -50,9 +54,12 @@ def generate_all_unwanted_pixel(bgFrame, hotStart):
 
 
 def read_all_unwanted_pixel(pathToBKGPIX, frameTemplate):
-    """This function reads out all pixels from the BKGPIX which do not belong
-    to the data set into a linear array."""
-    """These linear arrays can be used for fancy indexing."""
+    """This function reads out all pixels from the BKGPIX which do not belong to the data set into a linear array.
+    These linear arrays can be used for fancy indexing.
+    pathToBKGPIX ... string path to the BKGPIX.h5 file
+    frameTemplate ... fabio.Image placeholder to get the shape of the frame
+    returns numpy.array1d one-dimensional representation of the location of untrusted pixel
+    """
     untrustedPixel = []
     shapeX = frameTemplate.data.shape[0]
     shapeY = frameTemplate.data.shape[1]
@@ -60,21 +67,32 @@ def read_all_unwanted_pixel(pathToBKGPIX, frameTemplate):
     for x in range(shapeX):
         for y in range(shapeY):
             if bg[x, y] == BG_MASK:
+                # location of untrusted pixel if the frame is a linear array
                 untrustedPixel.append(x * frameTemplate.data.shape[1] + y)
     return np.asarray(untrustedPixel)
 
 
 def restore_simple_pixel_mask(frame, untrusted):
-    """Uses a linear array to change masked pixel to a new value."""
-    data = frame.data.flatten()
-    data[untrusted] = SIMPLE_UNTRUSTED
+    """Uses a linear array to change masked pixel to a new value.
+    frame ... fabio.Image the frame which needs to be restored
+    untrusted ... numpy.array1d one-dimensional representation of the location of untrusted pixel
+    returns numpy.array2d the masked data from the input frame
+    """
+    data = frame.data.flatten() # turns frame data into a 1d-array to allow fancy indexing
+    data[untrusted] = SIMPLE_UNTRUSTED # python's fancy indexing
     return np.reshape(data, frame.data.shape)
 
 
 def restore_pixel_mask(frame, defective, untrusted, hot):
-    """Uses linear arrays to return masked pixel to their original masked value."""
-    data = frame.data.flatten()
-    data[defective] = DEFECTIVE
+    """Uses a linear array to change masked pixel to a new value.
+    frame ... fabio.Image the frame which needs to be restored
+    defective ... numpy.array1d one-dimensional representation of the location of defective pixel
+    untrusted ... numpy.array1d one-dimensional representation of the location of untrusted pixel
+    hot ... numpy.array1d one-dimensional representation of the location of hot pixel
+    returns numpy.array2d the masked data from the input frame
+    """
+    data = frame.data.flatten() # turns frame data into a 1d-array to allow fancy indexing
+    data[defective] = DEFECTIVE # python's fancy indexing
     data[untrusted] = UNTRUSTED
     data[hot] = DEFECTIVE
     return np.reshape(data, frame.data.shape)
