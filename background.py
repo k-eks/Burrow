282f32b68850cdbfbae798bf7d44213fc9fd99ff
k-eks <kreks@gmx.at>
2016-03-24 14:40:59 +0100

    typo

--- background.py
+++ background.py
@@ -140,7 +140,7 @@ def generate_subframe_background_percentile(pathToFrames, pathToBackground, name
     outputModifiers = helping_tools.parse_substition_modifiers(outputModifiers)
     fileNames = []
     frameset = cbf_tools.Frameset(pathToFrames)
-    fileNames = frameset.generate_frame_names_from_template(pathToFrames)
+    fileNames = frameset.generate_frame_names_from_template()
 
     templateFrame = fabio.open(fileNames[0]) # just a prototype
     bg = np.zeros((templateFrame.data.shape[0], templateFrame.data.shape[1]))
@@ -241,7 +241,7 @@ def subtract_hybrid_background(pathToFrames, pathToSubtracted, backgroundFrameNa
     maskUntrusted, maskDefective, maskHot = cbf_tools.generate_all_unwanted_pixel(maskFrame, 1000000)
     print("starting subtracting\n")
     frameset = cbf_tools.Frameset(pathToFrames)
-    for fileName in frameset.generate_frame_names_from_template(pathToFrames):
+    for fileName in frameset.generate_frame_names_from_template():
         frame = fabio.open(fileName)
         frameFlux = cbf_tools.get_flux(frame)
         # mix the background frame
