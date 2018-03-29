8270ea467e1859f42009404339e847519d2a9b0e
k-eks <kreks@gmx.at>
2018-03-29 17:28:14 +0200

    added method to change the mask intensity an possibility to set the masked intensity in the base method.

--- puncher.py
+++ puncher.py
@@ -18,7 +18,7 @@ import glob
 MASKED_BRAGG_INTENSITY = -9999999
 
 
-def punch_Bragg(XDSPunchFilePath, sampleFrame, pathToFrames, frameNameTemplate, pathToPunched, punchedPrefix):
+def punch_Bragg(XDSPunchFilePath, sampleFrame, pathToFrames, frameNameTemplate, pathToPunched, punchedPrefix, maskedIntensity=MASKED_BRAGG_INTENSITY):
     """
     Masks all  Bragg peaks using XDS BGK files. For an unkown reason, when XDS's BKGPIX.cbf is substracted from a raw frame, then all positive intensities represent Bragg peaks.
     XDSPunchFilePath ... string location of the BKGPIX file create by XDS. NOTE: fabio can not read the that file by default, I used ALBULA to convert it to an h5 file!
@@ -27,6 +27,7 @@ def punch_Bragg(XDSPunchFilePath, sampleFrame, pathToFrames, frameNameTemplate, 
     frameNameTemplate ... string example of how the frames are named, allows for percent substitution.
     pathToPunched ... string location where the processed frames should be saved.
     punchedPrefix ... string prefix that should be put in front of the processed frames.
+    maskedIntensity ... int placeholder intensity which identifies it as masked.
     """
     frameShape = sampleFrame.data.shape
     # generating the background data
@@ -51,6 +52,32 @@ def punch_Bragg(XDSPunchFilePath, sampleFrame, pathToFrames, frameNameTemplate, 
         frame.write(os.path.join(pathToPunched, punchedPrefix + os.path.basename(fileName)))
         del frame # freeing memory
     print("\nPunching complete!")
+
+
+def update_masked_intensity(sampleFrame, pathToFrames, frameNameTemplate, pathToUpdated, updatedPrefix, updateIntensity=-1):
+    """
+    Changes the already provided masked intisities from MASKED_BRAGG_INTENSITY with to a new intensity.
+    sampleFrame ... fabio.frame some frame from which the dimensions can be read.
+    pathToFrames ... string location of the folder which contains the frames.
+    frameNameTemplate ... string example of how the frames are named, allows for percent substitution.
+    pathToPunched ... string location where the processed frames should be saved.
+    punchedPrefix ... string prefix that should be put in front of the processed frames.
+    """
+    frameShape = sampleFrame.data.shape
+
+    helping_tools.check_folder(pathToUpdated)
+    frameset = cbf_tools.Frameset(pathToFrames, frameNameTemplate)
+
+    # punching, the sorting function is used for a nice print out
+    for fileName in frameset.generate_frame_names_from_template():
+        print("Updating " + str(fileName), end="\r")
+        frame = fabio.open(fileName)
+        data = np.array(frame.data)
+        data[data <= MASKED_BRAGG_INTENSITY] = updateIntensity
+        frame.data = data
+        frame.write(os.path.join(pathToUpdated, updatedPrefix + os.path.basename(fileName)))
+        del frame # freeing memory
+    print("\nUpdating complete!")
 
 
 @helping_tools.deprecated
